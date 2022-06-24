6bd102f9476a122ba06d2b2ee6019554fe8bcabc
Labella Prieto <elabella@kpmg.es>
2022-06-24 16:21:39 +0200

    [MOD] Nuria - modifying files

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from model.nn import mlp
 if __name__ == "__main__":
     df = load_df_data("https://raw.githubusercontent.com/4data-lab/datasets/master/exercise.csv")
     X_train, X_test, y_train, y_test = split_data(df)
-    #  smote
-    #core, confusion_matrix, especifidad, sensibilidad = logistic_regression(X_train, y_train, X_test, y_test)
+    score, confusion_matrix, especifidad, sensibilidad = logistic_regression(X_train, y_train, X_test, y_test)
     score1, confusion_matrix1, especifidad1, sensibilidad1 = svm(X_train, y_train, X_test, y_test)
-    #score2, confusion_matrix2, especifidad2, sensibilidad2 = rf(X_train, y_train, X_test, y_test)
-    #score3, confusion_matrix3, especifidad3, sensibilidad3 = mlp(X_train, y_train, X_test, y_test)
-
-    print(df)
+    score2, confusion_matrix2, especifidad2, sensibilidad2 = rf(X_train, y_train, X_test, y_test)
+    score3, confusion_matrix3, especifidad3, sensibilidad3 = mlp(X_train, y_train, X_test, y_test)
--- validation/validation.py
+++ validation/validation.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from numpy import ndarray
 from sklearn import model_selection
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 
 def scoring(X_test: pd.DataFrame, y_test: pd.DataFrame, model, predictions: ndarray):
@@ -22,4 +25,17 @@ def scoring(X_test: pd.DataFrame, y_test: pd.DataFrame, model, predictions: ndar
     #Sensivilidad
     sensibilidad = VP/(VP+FN)*100
     print("Sensibilidad: "+str((VP/(VP+FN))*100))
+    RFP, RVP, umbrales = roc_curve(y_test, predictions)
+
+    sns.lineplot(x=RFP, y=RVP)
+    sns.lineplot(x=[0, 1], y=[0, 1], color="black", lw=1, linestyle="--")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.title("Espesificidad\n1.0                0.8                0.6                0.4                0.2                0.0", fontsize=10)
+    plt.xlabel("1 - Espesificidad = Ratio de falsos positivos")
+    plt.ylabel("Sensibilidad = Ratio verdaderos positivos")
+    plt.grid(True)
+    print("AUC - Datos de test")
+    print(roc_auc_score(y_test, predictions))
     return score, conf_matrix, especifidad, sensibilidad
+
